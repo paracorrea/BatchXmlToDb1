c53e2a0ad271a8de566d9239705c4512dbe0602a
fernando correa <paracorrea@gmail.com>
2022-05-30 14:21:36 -0300

    inicio

---
+++ src/main/java/flc/batch/BatchXmlToDb/BatchXmlToDbApplication.java
@@ -0,0 +1,13 @@
+package flc.batch.BatchXmlToDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchXmlToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchXmlToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flc/batch/BatchXmlToDb/BatchXmlToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package flc.batch.BatchXmlToDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchXmlToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
